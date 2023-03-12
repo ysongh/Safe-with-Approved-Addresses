2fcaede8cba138b8182ea093eb5897351148bf65
You Song Hou <ysongweb3@gmail.com>
2023-03-11 20:41:57 -0500

    feat: add a feature to approve an address to use the safe contract

--- contracts/AddressApprove.sol
+++ contracts/AddressApprove.sol
@@ -5,16 +5,40 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract AddressApprove {
     address public owner;
 
+    mapping(address => bool) public isAddressApprove;
+
     constructor(address _owner) {
         owner = _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    modifier onlyApprove() {
+        require(isAddressApprove[msg.sender], "You are not approved");
+        _;
     }
 
     function getETHBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function withdrawTokenfromSafe(uint _tokenAmount, address _tokenAddress) public {
+    function approveAddress(address _to) external onlyOwner {
+        isAddressApprove[_to] = true;
+    }
+
+    function revokeAddress(address _to) external onlyOwner {
+        isAddressApprove[_to] = false;
+    }
+
+    function withdrawTokenfromSafe(uint _tokenAmount, address _tokenAddress) external onlyApprove {
         IERC20 token = IERC20(_tokenAddress);
         require(token.transfer(owner, _tokenAmount), "Unable to transfer");    
+    }
+
+    function withdrawETH() external onlyApprove {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
